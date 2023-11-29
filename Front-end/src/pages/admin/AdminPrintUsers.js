3e9bf04e805d117bfa0e97c25f7c1d0ccafb0511
matthewdefilippo <105139525+matthewdefilippo@users.noreply.github.com>
2023-11-29 16:02:20 -0700

    Updated AdminPrintUsers page to display all registered users in the database.

--- Front-end/src/pages/admin/AdminPrintUsers.js
+++ Front-end/src/pages/admin/AdminPrintUsers.js
@@ -1,15 +1,51 @@
-import React from "react";
-import { Container, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import axios from "axios";
 
 const PrintUsers = () => {
-  // Future: Display list of registered users
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch all users when the component mounts
+    axios
+      .get("http://localhost:8080/api/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
   return (
     <Container maxWidth="lg">
       <Typography variant="h4" sx={{ mt: 4, mb: 4 }}>
         Registered Users
       </Typography>
-      {/* Future: Display list of registered users here */}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
